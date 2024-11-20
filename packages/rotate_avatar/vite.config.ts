88ddfb566b0c6b079842f4fd1a58f4fe9071ca2e
yukiice <58852690+yukiice@users.noreply.github.com>
2024-11-20 14:44:37 +0800

    add:修改初始化目录

--- packages/rotate_avatar/vite.config.ts
+++ packages/rotate_avatar/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import * as path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    port: 3000,
+    host:true
+  },
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'assets': path.resolve(__dirname, 'src/assets')
+    }
+  }
 })
